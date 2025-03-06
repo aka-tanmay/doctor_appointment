6d88605e59062154b82b12e26486ae2e8dd08152
tanmay <tulsidas.sg2002@gmail.com>
2025-03-06 22:20:04 +0530

    Integrated user Edit API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import {v2 as cloudinary} from 'cloudinary'
 
 
 // API to register user
@@ -91,9 +92,60 @@ const loginUser = async (req, res) => {
 
 // API to get user profile data
 
-const getProfile = async() =>{
-    
+const getProfile = async(req,res) =>{
+
+    try {
+        
+        const {userId} = req.body
+        const userData = await userModel.findById(userId).select('-password')
+
+        res.json({success:true,userData})
+        return
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+        return
+
+        
+    }
+
 }
 
 
-export{registerUser,loginUser}
+// API to update user profile 
+
+const updateProfile = async (req,res) => {
+
+    try {
+        
+        const {userId, name,phone,address,dob,gender} = req.body
+        const imageFile = req.file
+
+        if (!name || !phone || !dob || !gender) {
+            return res.json({success:false,message:"Data Missing"})
+            
+        }
+
+        await userModel.findByIdAndUpdate(userId,{name,phone,address:JSON.parse(address),dob,gender})
+
+        if (imageFile) {
+            
+            //uplode image to cloudnary
+            const imageUploade = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+            const imageURL = imageUploade.secure_url
+
+            await userModel.findByIdAndUpdate(userId,{image:imageURL})
+        }
+
+        res.json({success:true,message:"Profile Updated"})
+    } catch (error) {
+        
+        console.log(error)
+        res.json({success:false,message:error.message})
+        return
+    }
+}
+
+
+export{registerUser,loginUser,getProfile,updateProfile}
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,26 +1,70 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+
+import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
-function MyProfile() {
+const MyProfile = () => {
 
-  const [userData, setUserData] = useState({
-    name: "Sanket Shirke",
-    image: assets.profile_pic,
-    email: 'shirke@gmail.com',
-    phone: '+91 8301252020',
-    address: {
-      line1: "flat no 5 , starling apartment",
-      line2: "warje,pune"
-    },
-    gender: 'Male',
-    dob: '2002-01-20'
-  })
+  const {userData,setUserData,token,backendUrl,loadUserProfileData} = useContext(AppContext)
 
   const [isEdit, setIsEdit] = useState(false)
-  return (
+  const [image,setImage] = useState(false)
+
+  const updateUserProfileData = async ()=>{
+     
+   try {
+    
+    const formData = new FormData()
+
+    formData.append('name',userData.name)
+    formData.append('phone',userData.phone)
+    formData.append('address',JSON.stringify(userData.address))
+    formData.append('gender',userData.gender)
+    formData.append('dob',userData.dob)
+
+    image && formData.append('image',image)
+
+    const{data} = await axios.post(backendUrl + '/api/user/update-profile',formData,{headers:{token}})
+
+    if (data.success) {
+      toast.success(data.message)
+      await loadUserProfileData()
+      setIsEdit(false)
+      setImage(false)
+    }else{
+      toast.error(data.message)
+    }
+
+
+
+   } catch (error) {
+     
+     console.log(error)
+     toast.error(error.message)
+      
+
+   }
+
+  }
+
+  return userData && (
     <div className='max-w-lg flex flex-col gap-2 text-sm'>
 
-      <img  className='w-36 rounded'src={userData.image} alt="" />
+      {
+        isEdit
+        ? <label htmlFor="image">
+          <div className='inline-block relative cursor-pointer'>
+            <img className='w-36 rounded opacity-75'src={image ? URL.createObjectURL(image):userData.image} alt="" />
+            <img className='w-10 absolute bottom-12 right-12'src={image ? '' : assets.upload_icon} alt="" />
+          </div>
+          <input onChange={(e)=>setImage(e.target.files[0])}type="file" id="image" hidden/>
+        </label>
+        :      <img  className='w-36 rounded'src={userData.image} alt="" />
+
+      }
+
 
       {
         isEdit
@@ -93,7 +137,7 @@ function MyProfile() {
       <div className='mt-10'>
         {
           isEdit 
-          ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(false)}>Save information</button>
+          ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={updateUserProfileData}>Save information</button>
           : <button  className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)}>Edit</button>
         }
       </div>
