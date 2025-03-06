83b6634dc9405ef1b962206c21f3e09b59a2be7f
tanmay <tulsidas.sg2002@gmail.com>
2025-03-06 13:12:33 +0530

    Integrated login API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -54,4 +54,46 @@ const registerUser = async(req,res) => {
     }
 }
 
-export{registerUser}
+// API for user login 
+const loginUser = async (req, res) => {
+
+    try {
+        
+        const{email,password} = req.body
+        const user = await userModel.findOne({email})
+        
+        if (!user) {
+
+            res.json({success:false,message:'user does not exist'})
+            return
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+        
+        if (isMatch) {
+            
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+            res.json({success:true,token})
+            return
+            
+        }else{
+            res.json({success:false,message:"Invalid credentials"})
+            return
+        }
+
+    } catch (error) {
+        
+        console.log(error)
+        res.json({success:false,message:error.message})
+        return
+    }
+}
+
+// API to get user profile data
+
+const getProfile = async() =>{
+    
+}
+
+
+export{registerUser,loginUser}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,15 +9,13 @@ const AppContextProvider = (props) => {
 
   const currencySymbol = '₹'
   const backendUrl = import.meta.env.VITE_BACKEND_URL
+
   const [doctors,setDoctors] = useState([])
+  const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'): false)
+
    
   
 
-  const value ={
-       doctors,
-       currencySymbol
-
-  }
 
    const getDoctorsData = async ()=>{
 
@@ -34,7 +32,15 @@ const AppContextProvider = (props) => {
       console.log(error)
       toast.error(error.message)
     }
-   }
+   } 
+
+   const value ={
+    doctors,
+    currencySymbol,
+    token,setToken, 
+    backendUrl
+
+}
 
    useEffect(()=>{
         getDoctorsData()
