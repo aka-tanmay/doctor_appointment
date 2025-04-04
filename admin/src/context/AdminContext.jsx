808e83acbc6a9d1cd98cb6ee8c851a85b3997ff5
tanmay <tulsidas.sg2002@gmail.com>
2025-04-04 23:33:42 +0530

    Api to fetch admin dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
     const[aToken,setAToken] = useState(localStorage.getItem('aToken') ?  localStorage.getItem('aToken') : '')
     const [doctors,setdoctors] = useState([])
     const [appointments,setAppointments] = useState([])
+    const[dashData,setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -91,6 +92,28 @@ const AdminContextProvider = (props) => {
         }
     }
 
+  
+     const getDashData = async () => {
+
+        try {
+            
+            const{data} = await axios.get(backendUrl+'/api/admin/dashboard',{headers:{aToken}})
+
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+
+            
+        }
+     }
+
+
     const value = {
 
         aToken,setAToken,
@@ -98,7 +121,8 @@ const AdminContextProvider = (props) => {
         getAllDoctors,changedAvailability,
         appointments,setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,getDashData
 
     }
 
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -5,6 +5,7 @@ import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import appointmentModel from '../models/appointmentModel.js'
+import userModel from '../models/userModel.js'
 dotenv.config()
 // API for adding doctor 
 const addDoctor = async (req,res) => {
@@ -169,7 +170,30 @@ const appointementCancel = async (req, res) => {
   }
 };
 
+//API to get dashboard data for admin panel
+
+const adminDashboard = async (req,res) => {
+
+    try {
+        
+       const doctors = await doctorModel.find({})
+       const users = await userModel.find({})
+       const appointments = await appointmentModel.find({})
+
+       const dashData = {
+        doctors: doctors.length,
+        appointments:appointments.length,
+        patients:users.length,
+        latestAppointments: appointments.reverse().slice(0,5)
+       }
+
+       res.json({success:true,dashData})
+
+    } catch (error) {
+        console.log(error);
+    res.json({ success: false, message: error.message });
+    }
+}
 
 
-
-export {addDoctor,loginAdmin,allDoctors,appointmentsAdmin,appointementCancel}  //exporting all the functions to use in routes
+export {addDoctor,loginAdmin,allDoctors,appointmentsAdmin,appointementCancel,adminDashboard }  //exporting all the functions to use in routes
